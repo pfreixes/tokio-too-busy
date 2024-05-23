0ede2aae564a9ffb575cfdfda9d9a04a6b768360
Pau Freixes <pfreixes@github.com>
2024-05-23 11:51:50 +0200

    Documentation

---
+++ README.md
@@ -0,0 +1,39 @@
+Identify when the Tokio workers are too busy to handle more work.
+
+# Monitor the Tokio workers' busy ratio
+`TooBusy` allows you to track the busy ratio of the Tokio workers and react accordingly
+when they are too busy.
+
+In the example below, a `TooBusy` is built and used to evaluate the busy ratio of the Tokio workers 
+and progressively reject requests when the busy ratio goes above the low watermark threshold:
+
+```rust
+async fn my_middleware(State(state): State<AppState>, request: Request, next: Next) -> Response {
+   if state.too_busy.eval() {
+       return (StatusCode::SERVICE_UNAVAILABLE, "Server is too busy").into_response();
+   }
+   next.run(request).await
+}
+
+#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
+async fn main() {
+   let too_busy = TooBusy::builder()
+       .interval(Duration::from_secs(1))
+       .ewma_alpha(0.1)
+       .high_watermark(95)
+       .low_watermark(90)
+       .build();
+
+   let state = AppState { too_busy };
+
+   let app = Router::new()
+      .route("/", get(root))
+      .route_layer(middleware::from_fn_with_state(state.clone(), my_middleware))
+      .with_state(state);
+
+  let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+  axum::serve(listener, app).await.unwrap();
+}
+ ```
+
+Check the full [http example](./examples/http_server.rs)
--- src/lib.rs
+++ src/lib.rs
@@ -1,21 +1,94 @@
+//! Identify when the Tokio workers are too busy to handle more work.
+//!
+//! ### Monitor the Tokio workers' busy ratio
+//! [TooBusy] allows you to track the busy ratio of the Tokio workers and react accordingly
+//! when they are too busy.
+//!
+//! In the example below, a [`TooBusy`] is [built][TooBusy::builder] and used to
+//! [evaluate][TooBusy::eval] the busy ratio of the Tokio workers and progressively reject requests
+//! when the busy ratio goes above the [low watermark][TooBusyBuilder::low_watermark] threshold:
+//! ```
+//!async fn my_middleware(State(state): State<AppState>, request: Request, next: Next) -> Response {
+//!
+//!   if state.too_busy.eval() {
+//!       return (StatusCode::SERVICE_UNAVAILABLE, "Server is too busy").into_response();
+//!   }
+//!   next.run(request).await
+//!}
+//!
+//!#[tokio::main(flavor = "multi_thread", worker_threads = 1)]
+//!async fn main() {
+//!   let too_busy = TooBusy::builder()
+//!       .interval(Duration::from_secs(1))
+//!       .ewma_alpha(0.1)
+//!       .high_watermark(95)
+//!       .low_watermark(90)
+//!       .build();
+//!
+//!   let state = AppState { too_busy };
+//!
+//!   let app = Router::new()
+//!      .route("/", get(root))
+//!      .route_layer(middleware::from_fn_with_state(state.clone(), my_middleware))
+//!      .with_state(state);
+//!
+//!  let listener = tokio::net::TcpListener::bind("0.0.0.0:8080").await.unwrap();
+//!  axum::serve(listener, app).await.unwrap();
+//!}
+//! ```
+//! Check the full example in the [examples] directory.
+//!
+//! Tokio too busy leverages the [worker_total_busy_duration] interface provided by the Tokio [Runtime] for knowledge.
+//! and its only available when Tokio is compiled using the following flags ```RUSTFLAGS="--cfg tokio_unstable"```.
+//!
+//! Busy ratio is calculated by averaging the busy duration of all workers since latest iteration and using an 
+//! exponential moving average (EWMA).
+//!
+//! [examples]: https://github.com/pfreixes/tokio-too-busy/tree/main/examples
+//! [worker_total_busy_duration]: https://docs.rs/tokio/1.0.1/tokio/runtime/struct.Runtime.html#method.worker_total_busy_duration
+//! [Runtime]: https://docs.rs/tokio/1.0.1/tokio/runtime/struct.Runtime.html
 mod inner;
 use crate::inner::{LoadFeeder, TooBusyShared};
 use std::sync::Arc;
 use tokio::time::Duration;
 
+/// Track how busy are the tokio workers.
+///
+/// This type is internally reference-counted and can be freely cloned.
 pub struct TooBusy {
     inner: Arc<TooBusyShared>,
 }
 
 impl TooBusy {
+    /// Create a new builder to configure the [`TooBusy`] instance.
+    ///
+    /// # Examples
+    ///
+    /// ```
+    /// use tokio_too_busy::TooBusy;
+    /// use std::time::Duration;
+    ///
+    /// let too_busy = TooBusy::builder()
+    ///     .interval(Duration::from_secs(1))
+    ///     .ewma_alpha(0.1)
+    ///     .high_watermark(95)
+    ///     .low_watermark(90)
+    ///     .build();
+    /// ```
     pub fn builder() -> TooBusyBuilder {
         TooBusyBuilder::default()
     }
 
+    /// Get the current busy ratio of the Tokio workers.
     pub fn ratio_busy_ewma(&self) -> u32 {
         self.inner.ratio_busy_ewma()
     }
 
+    /// Evaluate if the Tokio workers are too busy.
+    ///
+    /// Returns `true` if the busy ratio is above the low watermark threshold using
+    /// a progressive rejection strategy until reach the high watermark threshold, above
+    /// the high watermark all evaluations will return `true`. Otherwise, it returns `false`.
     pub fn eval(&self) -> bool {
         self.inner.eval()
     }
@@ -29,6 +102,7 @@ impl Clone for TooBusy {
     }
 }
 
+/// This builder allows you to configure the [`TooBusy`] instance.
 pub struct TooBusyBuilder {
     interval: Duration,
     low_watermark: u32,
@@ -37,11 +111,16 @@ pub struct TooBusyBuilder {
 }
 
 impl TooBusyBuilder {
+    /// Set the interval to update the busy ratio.
+    ///
+    /// By default, it is 1 seconds.
     pub fn interval(mut self, interval: Duration) -> TooBusyBuilder {
         self.interval = interval;
         self
     }
-
+    /// Set the low watermark threshold to start considering the workers too busy.
+    ///
+    /// By default, it is 85%.
     pub fn low_watermark(mut self, low_watermark: u32) -> TooBusyBuilder {
         assert!(
             low_watermark < self.high_watermark,
@@ -50,7 +129,9 @@ impl TooBusyBuilder {
         self.low_watermark = low_watermark;
         self
     }
-
+    /// Set the high watermark threshold to start considering the workers too busy all the time.
+    ///
+    /// By default, it is 95%.
     pub fn high_watermark(mut self, high_watermark: u32) -> TooBusyBuilder {
         assert!(
             high_watermark > self.low_watermark,
@@ -59,7 +140,10 @@ impl TooBusyBuilder {
         self.high_watermark = high_watermark;
         self
     }
-
+    /// Set the alpha value for the EWMA calculation. 
+    ///
+    /// By default, it is 0.1 (10%) for having quick reactions to changes. If you want to smooth the changes
+    /// you can set a higher value.
     pub fn ewma_alpha(mut self, ewma_alpha: f32) -> TooBusyBuilder {
         assert!(
             ewma_alpha > 0.0 && ewma_alpha < 1.0,
@@ -68,7 +152,7 @@ impl TooBusyBuilder {
         self.ewma_alpha = ewma_alpha;
         self
     }
-
+    /// Build the [`TooBusy`] instance.
     pub fn build(self) -> TooBusy {
         let inner = Arc::new(TooBusyShared::new(self.low_watermark, self.high_watermark));
         let weak_reference = Arc::downgrade(&inner);
@@ -86,7 +170,7 @@ impl TooBusyBuilder {
 impl Default for TooBusyBuilder {
     fn default() -> Self {
         TooBusyBuilder {
-            interval: Duration::from_secs(5),
+            interval: Duration::from_secs(1),
             low_watermark: 85,
             high_watermark: 95,
             ewma_alpha: 0.1,
